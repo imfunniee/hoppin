dfb2d9b7467fd0a120812a0189fd9401445e7deb
funny <36105478+imfunniee@users.noreply.github.com>
2022-07-26 23:31:57 +0530

    Initial commit

--- next.config.js
+++ next.config.js
@@ -1,11 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-  env: {
-    PROJECT_ID: process.env.PROJECT_ID,
-    CHANNEL_ID: process.env.CHANNEL_ID,
-  },
+	reactStrictMode: true,
+	swcMinify: true,
+	env: {
+		PROJECT_ID: process.env.PROJECT_ID,
+		CHANNEL_ID: process.env.CHANNEL_ID,
+	},
 };
 
 module.exports = nextConfig;
